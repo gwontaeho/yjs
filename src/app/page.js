e2f13b46ddbc65ec44498a6f10892488b3d176e5
gwontaeho <taeho8199@gmail.com>
2025-06-17 17:15:59 +0900

    ;

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
+import { createPortal } from "react-dom";
 
 const Editor = dynamic(() => import("@/components/Editor"), { ssr: false });
 
@@ -9,12 +10,15 @@ export default function Home() {
   const [current, setCurrent] = useState(null);
   const [docs, setDocs] = useState([]);
 
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     getDocuments();
   }, []);
 
   const getDocuments = async () => {
     try {
+      setLoading(true);
       const res = await fetch(
         "https://yjs-server-9z38.onrender.com/api/documents"
       );
@@ -22,6 +26,8 @@ export default function Home() {
       setDocs(docs);
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -83,6 +89,33 @@ export default function Home() {
       </div>
 
       {current && <Editor name={current} />}
+
+      {loading &&
+        createPortal(
+          <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black/10">
+            <svg
+              className="mr-3 -ml-1 size-5 animate-spin text-black"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          </div>,
+          document.body
+        )}
     </div>
   );
 }
